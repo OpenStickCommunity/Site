7512f5d38702ba99c1f0eece65cb5c0217105006
Luke Arntson <arntsonl@gmail.com>
2023-12-29 17:47:11 -0500

    Pushing up to 0.7.6

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@
 const lightCodeTheme = require('prism-react-renderer').themes.github;
 const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
-const releaseVersion = '0.7.5';
+const releaseVersion = '0.7.6';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
