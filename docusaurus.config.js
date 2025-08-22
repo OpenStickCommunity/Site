b246cf4cd7c0214cbe79c8068b0bee5101f7dea7
McGuffinCN <superass@126.com>
2025-08-22 11:31:24 +0800

    Changes to be committed: 	modified:   docusaurus.config.js 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/add-ons/analog.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/add-ons/keyboard-host.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/add-ons/wii-extensions.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/mini-menu.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/snippets/_add-usb-host-port.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/snippets/_hotkey-note.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/snippets/_pin-options-USB-Host.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/usage.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/web-configurator/menu-pages/00-home.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/web-configurator/menu-pages/01-settings.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/web-configurator/menu-pages/02-gpio-pin-mapping.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/web-configurator/menu-pages/03-peripheral-mapping.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/web-configurator/menu-pages/05-led-configuration.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/web-configurator/menu-pages/06-custom-led-theme.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/web-configurator/menu-pages/07-display-configuration.mdx 	modified:   i18n/zh/docusaurus-plugin-content-docs/current/web-configurator/menu-pages/10-data-backup-restoration.mdx

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
       },
       announcementBar: {
         id: "new_release",
-        content: `New Version Available! To get the v${releaseVersion} update, go to <a href="https://github.com/OpenStickCommunity/GP2040-CE/releases/tag/v${releaseVersion}" target="_blank">GP2040-CE Releases<a/>`,
+        content: `New Version Available! To get the v${releaseVersion} update, go to <a href="https://github.com/OpenStickCommunity/GP2040-CE/releases/tag/v${releaseVersion}" target="_blank">GP2040-CE Releases</a>`,
         backgroundColor: "#ec008c",
         textColor: "#FFFFFF",
         isCloseable: true,
